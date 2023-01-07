349ed92f5b6c56ebef22b4919f746d85363c2ec7
Baris Findik <baris.findik@getir.com>
2023-01-07 18:12:31 +0300

    login and register page

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
-    path("<int:pk>/", views.project_detail, name="project_detail"),
     path("register", views.register_request, name="register"),
+    path("", views.login_request, name="login"),
     path("login", views.login_request, name="login"),
 ]
--- finalproject/views.py
+++ finalproject/views.py
@@ -6,21 +6,6 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm 
 
-def project_index(request):
-    projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render(request, 'project_index.html', context)
-
-def project_detail(request, pk):
-    project = Project.objects.get(pk=pk)
-    context = {
-        'project': project
-    }
-    return render(request, 'project_detail.html', context)
-
-
 
 def register_request(request):
 	if request.method == "POST":
