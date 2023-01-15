e9021d6030c909dd070a13e0517086eac1bc878f
Baris Findik <baris.findik@getir.com>
2023-01-15 19:54:08 +0300

    url is corrected

--- finalproject/config.py
+++ finalproject/config.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
-DB_URL = 'postgres://sdbzvxegowxftb:1dc5433255ead59faed22eb2544fb84008f9c0067e4e2fac6e4c28fa290ade93@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dehmh5f8fepnth'
+DB_URL = 'postgresql://sdbzvxegowxftb:1dc5433255ead59faed22eb2544fb84008f9c0067e4e2fac6e4c28fa290ade93@ec2-107-23-76-12.compute-1.amazonaws.com:5432/dehmh5f8fepnth'
 DB_ENGINE = create_engine(DB_URL)
